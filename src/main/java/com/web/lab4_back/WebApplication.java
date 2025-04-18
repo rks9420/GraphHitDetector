f30b18a2d1f07ee190edfe6d4ff5727b6f23a05d
Ivanov Ilya <iiilya12@yandex.ru>
2025-04-18 15:55:46 +0300

    add app class

---
+++ src/main/java/com/web/lab4_back/WebApplication.java
@@ -0,0 +1,10 @@
+package com.web.lab4_back;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+
+@ApplicationPath("/api")
+public class WebApplication extends Application {
+}
